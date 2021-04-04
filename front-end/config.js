83cf3acaed6e97639531b24fceeec07d42d03351
lemon <601749122@qq.com>
2021-04-04 10:07:18 +0800

    change smartcontract && use server database

--- front-end/config.js
+++ front-end/config.js
@@ -1,4 +1,5 @@
-const databaseUrl = "mongodb://127.0.0.1:27017/Degree";
+//const databaseUrl = "mongodb://127.0.0.1:27017/Degree";
+const databaseUrl = "mongodb+srv://xinyi:xinyi@degree.6b0qs.mongodb.net/Degree";
 module.exports={
 	databaseUrl:databaseUrl
 }
