265ef3593a547d3930102fd71fd49c548c44868a
Genji.Teen <601749122@qq.com>
2021-04-28 10:43:27 +0800

    Update config.js

--- front-end/config.js
+++ front-end/config.js
@@ -1,5 +1,5 @@
-// const databaseUrl = "mongodb://127.0.0.1:27017/Degree";
-const databaseUrl = "mongodb+srv://xinyi:xinyi@degree.6b0qs.mongodb.net/Degree?retryWrites=true&w=majority";
+const databaseUrl = "mongodb://127.0.0.1:27017/Degree";
+//const databaseUrl =
 const emailConfig={
 	service:"qq",
 	email:"emailexalple@qq.com",
