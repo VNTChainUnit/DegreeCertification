1e352f6c7c4dc8eb4b080597fbd45c29607b9ede
Genji.Teen <601749122@qq.com>
2021-04-28 10:45:51 +0800

    Update common.js

--- front-end/service/blockchain/common.js
+++ front-end/service/blockchain/common.js
@@ -2,7 +2,7 @@ const abifile="Degree.abi"
 const contractAddress="0xe9907f03ba3751d77213f671ff00211c0212bbf3"
 const url="http://47.111.100.232:8880"
 const accountPath="./owneraccount"
-const accountPassword=""
+const accountPassword="Your password"
 module.exports={
     abifile:abifile,
     url:url,
