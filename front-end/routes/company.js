91c9db6269fec16324e4c60a9c7c0283d5b293df
lemon <601749122@qq.com>
2022-01-12 10:41:32 +0800

    #6 Finish back-end

--- front-end/routes/company.js
+++ front-end/routes/company.js
@@ -4,6 +4,37 @@ const companyService=require('../service/companyService')
 const blockchain=require('../service/blockchain/main');
 const utils = require('../service/utils');
 const applicationService=require('../service/applicationService')
+var multer  = require('multer')
+var fs = require('fs');
+const xlsx = require('node-xlsx')
+var path=require('path');
+//***********文件上传配置begin
+
+var createFolder = function(folder){
+  try{
+      fs.accessSync(folder); 
+  }catch(e){
+      fs.mkdirSync(folder);
+  }  
+};
+
+var uploadFolder = './upload/excel';
+
+createFolder(uploadFolder);
+// 通过 filename 属性定制
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+      cb(null, uploadFolder);    // 保存的路径，备注：需要自己创建
+  },
+  filename: function (req, file, cb) {
+      // 将保存文件名设置为 字段名 + 时间戳，比如 logo-1478521468943
+      cb(null, Date.now()+"."+"xls");  
+  }
+});
+
+var upload = multer({ storage: storage });
+//***********文件上传配置end
+
 //身份验证
 router.use('/', (req, res, next) => {
   if (req.session.username!=null && req.session.usertype==2) {
@@ -80,6 +111,53 @@ router.post('/api/application/status',(req,res,next)=>{
       applicationService.openApplication(req.body.applicationid);
     }
     res.json(utils.restful(null,null,null))
+})
+
+/**
+ * 上传批量核验excel，返回核验结果
+ */
+router.post('/api/uploadCheckMultipleCertificate',upload.single('file'),async function(req,res,next){
+  var file = req.file;
+  let filePath=path.join(__dirname,'../upload/excel/'+file.filename)
+  let sheetList = xlsx.parse(filePath);
+  //第8行开始取数据，证书编号、姓名、学校名称、专业名称、学位类别、毕业日期
+  if(sheetList[1]==null){
+    //TODO 返回错误
+  }
+  let head = ['证书编号','姓名','学校名称','专业名称','学位类别','毕业日期','核验结果'];
+  let out_data = [];
+  out_data.push(head);
+  let excelData = xlsx.parse(filePath);
+  const data=excelData[0].data;
+  //从第8行开始
+  data.forEach((item, index) => {
+      if (index <= 6 || item == null || item.length==0 ) {
+        // 去除前7行
+        return;
+      } else {
+        let result = blockchain.checkCertificate(item[0],item[1],item[2],item[4],item[5],item[3]);
+        if(result){
+          item.push('核验通过');
+        }
+        else{
+          item.push('核验不通过');
+        }
+        out_data.push(item);
+      }
+  });
+  let out_fileDir = path.join(__dirname,'../public/downloads/check/')
+  let out_fileName = Date.now()+'.xlsx';
+  let out_url = '/static/downloads/check/'+out_fileName;
+  let result = utils.excelExport(out_data,out_fileDir+out_fileName);
+  if(result){
+    res.json(utils.restful(null,{
+      'url':out_url
+    },null))
+  }
+  else{
+    res.json(utils.restful(-1,null,"System error!"))
+  }
+  
 })
 
 module.exports = router;
